c0935864bd4664200e749894576b8a3dd8bed314
Ian Gallardo <ian@iangallardo.com>
2025-08-27 18:26:57 -0400

    add unit test

---
+++ test/index.basic.test.js
@@ -0,0 +1,19 @@
+// test/index.basic.test.js
+const { expect } = require('chai');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const token = require('../config.json').token;
+
+describe('Discord bot basic setup', function () {
+  it('should create a Discord client with commands collection', function () {
+    const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+    client.commands = new Collection();
+
+    expect(client).to.be.an.instanceOf(Client);
+    expect(client.commands).to.be.instanceOf(Collection);
+  });
+
+  it('should have a token defined', function () {
+    expect(token).to.be.a('string').and.not.empty;
+  });
+});
