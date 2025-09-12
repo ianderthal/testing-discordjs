b909dd1196be51f5af4fc46066589ecd3e7760e9
Ian Gallardo <ian@iangallardo.com>
2025-09-12 17:39:50 -0400

    working embed for strava profile

--- commands/strava/strava-profile.js
+++ commands/strava/strava-profile.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { stravaFetch } = require("../../services/strava");
 
 module.exports = {
@@ -15,7 +15,20 @@ module.exports = {
 
       const jsonOutput = "```json\n" + JSON.stringify(profile, null, 2).slice(0, 1900) + "\n```";
 
-      await interaction.editReply(jsonOutput);
+      const exampleEmbed = new EmbedBuilder()
+        .setColor('FC4C02')
+        .setTitle(`Strava Profile - ${profile.firstname} ${profile.lastname}`)
+        .setURL('https://discord.js.org/')
+        .setAuthor({ name: 'Strava API', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+        .setDescription('Check out this sweet-ass embed from Strava with your profile information')
+        .addFields(
+          { name: 'Profile Information', value: jsonOutput }
+        )
+        .setTimestamp()
+        .setFooter({ text: 'Powered by Strava', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+      //channel.send({ embeds: [exampleEmbed] });
+      await interaction.editReply({ embeds: [exampleEmbed] });
     } catch(err) {
       console.error("Error fetching Strava profile:", err.message);
       await interaction.editReply("Could not fetch Strava profile.");
